a4d3fad40af47df812c376fd8e628eabfc70b2c8
breezedeus <breezedeus@163.com>
2016-05-03 16:46:53 +0800

    增加了

---
+++ run.sh
@@ -0,0 +1,11 @@
+# coding=utf8
+#!/bin/bash
+code_dir='jiayuan/cn_char_rnn'
+data_dir='data/qiushibaike'
+
+# tokenize raw jokes
+#python ${code_dir}/tokenize_cn.py ${data_dir}/input.txt ${data_dir}/tokenized/input.txt
+
+#python ${code_dir}/train.py --data_dir ${data_dir}/tokenized --use_ori 0 --num_epochs 1
+
+python ${code_dir}/sample.py --prim '我们 就是'

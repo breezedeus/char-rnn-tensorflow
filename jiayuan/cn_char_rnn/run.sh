3a659f9051ee94531a3acbcc6aac49d3ae15f288
breezedeus <breezedeus@163.com>
2016-05-11 11:25:33 +0800

    no big change

--- jiayuan/cn_char_rnn/run.sh
+++ jiayuan/cn_char_rnn/run.sh
@@ -8,9 +8,10 @@ save_dir="output_data/save"
 
 use_ori=1
 model="lstm"
-rnn_size=256
+rnn_size=512
 num_layers=1
 
+save_dir=${save_dir}_${rnn_size}
 python ${code_dir}/train.py --data_dir ${data_dir} --save_dir ${save_dir} --model ${model} \
         --use_ori ${use_ori} --num_epochs 10 --rnn_size ${rnn_size} --num_layers ${num_layers}
 

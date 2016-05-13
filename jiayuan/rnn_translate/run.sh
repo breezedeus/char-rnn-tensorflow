9f6ad9b5f0d0c5c4b0c091bdde2f7e521f90100d
breezedeus <breezedeus@163.com>
2016-05-13 14:58:02 +0800

    no big change

--- jiayuan/rnn_translate/run.sh
+++ jiayuan/rnn_translate/run.sh
@@ -5,10 +5,12 @@ cd `dirname $0`
 train_data_dir=data/qiuai
 output_dir=output_data
 
-size=128
+size=512
 num_layers=1
 decode=False
+use_ori=True
 
+output_dir="${output_dir}/save${size}_${num_layers}"
 python rnn_translate/translate.py --data_dir ${train_data_dir} --train_dir ${output_dir} \
                     --en_vocab_size 40000 --fr_vocab_size 40000 \
-                    --decode ${decode} --size ${size} --num_layers ${num_layers}
+                    --decode ${decode} --size ${size} --num_layers ${num_layers} -use_ori ${use_ori}

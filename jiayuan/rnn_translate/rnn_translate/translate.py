9ae1fa83025af569f647fd987881333897495136
breezedeus <breezedeus@163.com>
2016-05-15 00:55:37 +0800

    在decode时对输入引起的捕获的异常进行处理

--- jiayuan/rnn_translate/rnn_translate/translate.py
+++ jiayuan/rnn_translate/rnn_translate/translate.py
@@ -237,30 +237,34 @@ def decode():
     sys.stdout.flush()
     sentence = sys.stdin.readline()
     while sentence:
-      # Get token-ids for the input sentence.
-      token_ids = data_utils.sentence_to_token_ids(tf.compat.as_bytes(sentence), en_vocab, tokenizer=tokenizer)
-      if len(token_ids) >= _buckets[-1][0]:
-          token_ids = token_ids[-(_buckets[-1][0]-1):]
-      print(token_ids)
-      # Which bucket does it belong to?
-      bucket_id = min([b for b in xrange(len(_buckets))
-                       if _buckets[b][0] > len(token_ids)])
-      # Get a 1-element batch to feed the sentence to the model.
-      encoder_inputs, decoder_inputs, target_weights = model.get_batch(
-          {bucket_id: [(token_ids, [])]}, bucket_id)
-      # Get output logits for the sentence.
-      _, _, output_logits = model.step(sess, encoder_inputs, decoder_inputs,
-                                       target_weights, bucket_id, True)
-      # This is a greedy decoder - outputs are just argmaxes of output_logits.
-      outputs = [int(np.argmax(logit, axis=1)) for logit in output_logits]
-      # If there is an EOS symbol in outputs, cut them at that point.
-      if data_utils.EOS_ID in outputs:
-        outputs = outputs[:outputs.index(data_utils.EOS_ID)]
-      # Print out French sentence corresponding to outputs.
-      print(" ".join([tf.compat.as_str(rev_fr_vocab[output]) for output in outputs]))
-      print("> ", end="")
-      sys.stdout.flush()
-      sentence = sys.stdin.readline()
+      try:
+        # Get token-ids for the input sentence.
+        token_ids = data_utils.sentence_to_token_ids(tf.compat.as_bytes(sentence), en_vocab, tokenizer=tokenizer)
+        if len(token_ids) >= _buckets[-1][0]:
+            token_ids = token_ids[-(_buckets[-1][0]-1):]
+        print(token_ids)
+        # Which bucket does it belong to?
+        bucket_id = min([b for b in xrange(len(_buckets))
+                         if _buckets[b][0] > len(token_ids)])
+        # Get a 1-element batch to feed the sentence to the model.
+        encoder_inputs, decoder_inputs, target_weights = model.get_batch(
+            {bucket_id: [(token_ids, [])]}, bucket_id)
+        # Get output logits for the sentence.
+        _, _, output_logits = model.step(sess, encoder_inputs, decoder_inputs,
+                                         target_weights, bucket_id, True)
+        # This is a greedy decoder - outputs are just argmaxes of output_logits.
+        outputs = [int(np.argmax(logit, axis=1)) for logit in output_logits]
+        # If there is an EOS symbol in outputs, cut them at that point.
+        if data_utils.EOS_ID in outputs:
+            outputs = outputs[:outputs.index(data_utils.EOS_ID)]
+        # Print out French sentence corresponding to outputs.
+        print(" ".join([tf.compat.as_str(rev_fr_vocab[output]) for output in outputs]))
+      except Exception:
+        print("Bad input! Try again:")
+      finally:
+        print("> ", end="")
+        sys.stdout.flush()
+        sentence = sys.stdin.readline()
 
 
 def self_test():
